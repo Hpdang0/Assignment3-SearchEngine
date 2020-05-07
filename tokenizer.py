39af5f88e7a91cc74daff24e38463e89b1707ff9
HARSHCHANDRAYADAV <hcy1903@hotmail.com>
2020-05-06 17:30:22 -0700

    basic tokenizer
    the most basix tokenizer possible so that the program works.

--- tokenizer.py
+++ tokenizer.py
@@ -1,4 +1,23 @@
 import bs4
 
-def tokenize(content: str) -> [str]:
-    return []
+class tokenizer
+{
+    stopwords = []
+    file = open("stopwords.txt",'r')
+    lines = file.read().split()
+    for word in lines:
+        stopwords.append(word)
+
+    def tokenize(content: str) -> [str]:
+    {
+        token_list = []
+        for word in content:
+            word = word.lower().strip("!@#$%^&*(),-_=+./:;''\\][`")
+            if len(word) >3 :
+                if word not in self.stopwords:
+                    if word not in token_list:
+                        token_list.append(word)
+        return token_list
+    }
+    
+}
