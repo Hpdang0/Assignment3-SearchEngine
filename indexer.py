b60c7cf31f7a6cd569c9c4d2edf8d2419dbbbc2c
Christian Berry <ceberry@uci.edu>
2020-05-07 22:05:37 -0700

    added tinydb and clears index dict

--- indexer.py
+++ indexer.py
@@ -1,5 +1,7 @@
 import os
 from collections import defaultdict
+from tinydb import TinyDB, Query
+
 
 import jsonparse
 import tokenizer
@@ -10,9 +12,13 @@ _CORPUS_PATH = '\\DEV'
 
 
 if __name__ == '__main__':
+    db = TinyDB('index.json') # this is the file with the index info
+    db.purge() # THIS CLEARS THE FILE !!! (we probably want it to be blank a the start)
     index = defaultdict(list)
     doc_ids = defaultdict(int)
     tokenizer = tokenizer.Tokenizer()
+    threshhold = 5 # this is how many documents we go before writing to the file and clearing our local index
+
 
     current_doc_id = 0
 
@@ -36,3 +42,17 @@ if __name__ == '__main__':
             # Indexing
             # for token in tokens:
                 # index[token].append(current_doc_id)
+
+            for token in tokens:
+                index[token].append(current_doc_id)
+            
+        if current_doc_id >= threshhold:
+            threshhold += current_doc_id
+            for key in index.keys():
+                db.insert({'token' : key, 'list': index[key]}) # the 'token' will help us when we're searching tokens later
+            index = {} # clearing the dict
+    
+    for key in index.keys(): # one last addition
+        db.insert({'token' : key, 'list': index[key]})
+    index = {} # clearing the dict but it doesn't really matter at this point
+        
