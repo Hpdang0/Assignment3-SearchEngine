bc10ea8977829d8ee2c25d2c20f34a2f14ed4892
Christian Berry <ceberry@uci.edu>
2020-05-07 22:58:45 -0700

    added Pair class that works like an mutable tuple

--- indexer.py
+++ indexer.py
@@ -9,12 +9,22 @@ import wordprocess
 
 _CORPUS_PATH = '\\DEV'
 
+class Pair:
+    def __init__(self, doc, amount = 0):
+        self.doc = doc
+        self.amount = amount
+    
+    def inc(self):
+        self.amount += 1
+
+    def show(self):
+        return '('+str(self.doc) + ', ' + str(self.amount) + ')'
 
 
 if __name__ == '__main__':
     db = TinyDB('index.json') # this is the file with the index info, we will need another program to query
     db.purge() # THIS CLEARS THE FILE !!! (we probably want it to be blank a the start)
-    index = defaultdict(list)
+    index = {} # instead of default dict with list
     doc_ids = defaultdict(int)
     tokenizer = tokenizer.Tokenizer()
     threshhold = 5 # this is how many documents we go before writing to the file and clearing our local index
@@ -44,14 +54,18 @@ if __name__ == '__main__':
                 # index[token].append(current_doc_id)
 
             for token in tokens:
-                index[token].append(current_doc_id)
+                # index[token].append(current_doc_id)
+                if token in index.keys():
+                    index[token].inc
+                else:
+                    index[token] = Pair(current_doc_id, 1)
             
         if current_doc_id >= threshhold:
             threshhold += current_doc_id
             for key in index.keys():
-                db.insert({'token' : key, 'list': index[key]}) # the 'token' will help us when we're searching tokens later
+                db.insert({'token' : key, 'tuple': index[key].show()}) # the 'token' will help us when we're searching tokens later
             index.clear() # clearing the dict
     
     for key in index.keys(): # one last addition
-        db.insert({'token' : key, 'list': index[key]})
+        db.insert({'token' : key, 'tuple': index[key].show()})
     index.clear() # clearing the dict but it doesn't really matter at this point
