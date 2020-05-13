4d159f8d86eee9d5f858895d6d4c46527bcbdb08
blongty <btyang24@gmail.com>
2020-05-13 16:31:31 -0700

    Added timer to search method, type q to quit on console.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import time
 
 import tokenizer
 import search
@@ -15,18 +16,29 @@ if __name__ == '__main__':
     search = search.Search('final.index', 'final.ids')
     
     while True:
-        i = input('>> Query: ')
+        i = input('\n\n[Type "q" to quit]\n>> Query: ')
         print()
 
         # Ensure that query is alphanumeric
         if not ensure_legal(i):
-            print('[ERROR] Query must contain only alphanumeric characters.\n')
+            print('[ERROR] Query must contain only alphanumeric characters.')
             continue
+        if i == 'q':
+            break
 
         # Tokenize just like how we did for indexing
         query_tokens = tokenizer.tokenize_query(i)
 
-        # Search for results and print
+        # Time and do search
+        start = time.time()
+        results = search.search(query_tokens)
+        end = time.time()
+        print('[Query took {:.3f} seconds]'.format(end - start))
+        
+        # Print the results
         print('Results:')
-        for url in search.search(query_tokens):
+        for url in results:
             print(url)
+        
+        print()
+            
