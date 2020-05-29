f3c2c6ee658e07583c502c79a4fa083a6476b773
blongty <btyang24@gmail.com>
2020-05-28 20:39:47 -0700

    Similarity and low value filtering on Indexer, tokenizer now returns a dict of frequencies

--- indexer.py
+++ indexer.py
@@ -1,15 +1,22 @@
 import os
 from collections import defaultdict
 import sys
+import re
 
 import jsonparse
 import tokenizer
 import indexfiler
 from io import TextIOWrapper
-
+import cache
 import time
+from similar import Similarity
 
-_CORPUS_PATH = '\\DEV'
+CORPUS_PATH = '\\DEV'
+LOW_VALUE_THRESHOLD = 20
+SIMHASH_THRESH = 0.9
+
+_FRAGMENT = r'[#].*'
+FRAGMENT = re.compile(_FRAGMENT)
 
 def merge_indexes():
     filer = indexfiler.IndexFiler()
@@ -26,6 +33,7 @@ if __name__ == '__main__':
     # Class setup
     tokenizer = tokenizer.Tokenizer()
     filer = indexfiler.IndexFiler()
+    cache = cache.Cache()
     
     # File Writing setup
     current_tmp_index = 0
@@ -36,60 +44,80 @@ if __name__ == '__main__':
     index = defaultdict(list)
     doc_ids = defaultdict(int)
     frequency = defaultdict(int)
-    rem_filename = None
     unique_tokens = set()
     total_unique_tokens = 0
+    filename = None
     
-    current_doc_id = 0
+    current_doc_id = 1
     try:
         start = time.time()
-        for path, folder, filenames in os.walk(os.getcwd() + _CORPUS_PATH):
+        for path, folder, filenames in os.walk(os.getcwd() + CORPUS_PATH):
             for filename in filenames:
                 rem_filename = filename
-                frequency.clear()
                 if filename.endswith('.gitignore'):
                     continue
-                
-                current_doc_id += 1
-                
 
                 # Parse JSON here
                 url, content, encoding = jsonparse.parse(path + '\\' + filename)
                 doc_ids[current_doc_id] = url
 
+                if FRAGMENT.search(url) is not None:
+                    continue
+
                 # Tokenize Content
-                tokens = tokenizer.tokenize_index(content, encoding)
-                for token in tokens:
-                    unique_tokens.add(token)
-                total_unique_tokens = len(unique_tokens)
+                frequency = tokenizer.tokenize_index(content, encoding)
+
+                # -------------- Filtering done here
+                # Determine if indexing is worthwhile
+                low_value_page = False
+                if sum(frequency.values()) < LOW_VALUE_THRESHOLD:
+                    low_value_page = True
+                    print('[SKIPPING] URL found to be of low value: \n{0} tokens\nURL: {1}'.format(sum(frequency.values()), url))
+
+                # Compare similarity to last 5 pages we crawled in
+                similar = False
+                for url_token_pair in cache:
+                    # print(frequency, url_token_pair[1], sep='\n')
+                    if Similarity(frequency, url_token_pair[1], SIMHASH_THRESH):
+                        similar = True
+                        print('[SKIPPING] Similarity found between these two urls. Skipping the second url...\n{0}\n{1}'.format(url_token_pair[0], url))
+                        break
+                # ------------------------ End filter
+
+                # Remember this url in our cache
+                cache.append((url, frequency))
+
+                # Actually add to our index
+                if not similar and not low_value_page:
+                    # Put weights on frequency
+
+                    # Add to index on memory
+                    for token in frequency:
+                        index[token].append([current_doc_id, frequency[token]])
+
+                    # Logging
+                    if current_doc_id % 100 == 0:
+                        print('{:.2f} Processed up to doc_id: {}\nName: {}\nIndex Size: {}\nUnique Tokens: {}\n'.format(time.time() - start, current_doc_id, url, sys.getsizeof(index), total_unique_tokens))
+
+                    # Write of index
+                    if sys.getsizeof(index) >= write_threshhold:
+                        print('>> Writing tmp_{}.index with index of size {}'.format(current_tmp_index, sys.getsizeof(index)))
+                        
+                        filer.index_to_file(index, 'tmp_{}.index'.format(current_tmp_index))
+                        current_tmp_index += 1
+
+                        index.clear()
+
+                    # Writing of doc_ids
+                    if sys.getsizeof(doc_ids) >= write_threshhold:
+                        print('>> Writing final.ids with doc ids of size {}'.format(sys.getsizeof(doc_ids)))
+
+                        filer.ids_to_file(doc_ids, 'final.ids')
+                        current_tmp_ids += 1
                 
-                # Convert to indexable frequency
-                for token in tokens:
-                    frequency[token] += 1
-
-                # Add to index on memory
-                for token in frequency:
-                    index[token].append([current_doc_id, frequency[token]])
-
-                # Logging
-                if current_doc_id % 100 == 0:
-                    print('{:.2f} Processed up to doc_id: {}\nName: {}\nIndex Size: {}\nUnique Tokens: {}\n'.format(time.time() - start, current_doc_id, filename, sys.getsizeof(index), total_unique_tokens))
-
-                # Write of index
-                if sys.getsizeof(index) >= write_threshhold:
-                    print('>> Writing tmp_{}.index with index of size {}'.format(current_tmp_index, sys.getsizeof(index)))
-                    
-                    filer.index_to_file(index, 'tmp_{}.index'.format(current_tmp_index))
-                    current_tmp_index += 1
-
-                    index.clear()
-
-                # Writing of doc_ids
-                if sys.getsizeof(doc_ids) >= write_threshhold:
-                    print('>> Writing tmp_{}.ids with doc ids of size {}'.format(current_tmp_ids, sys.getsizeof(doc_ids)))
-
-                    filer.ids_to_file(doc_ids, 'tmp_{}.ids'.format(current_tmp_ids))
-                    current_tmp_ids += 1
+                # Prepare for next document
+                current_doc_id += 1
+                frequency.clear()
 
         
         #here is where we would put the "HELPER"
@@ -104,10 +132,10 @@ if __name__ == '__main__':
     print('>> Writing tmp_{}.index with index of size {}'.format(current_tmp_index, sys.getsizeof(index)))
     filer.index_to_file(index, 'tmp_{}.index'.format(current_tmp_index))
     
-    print('>> Writing tmp_{}.ids with doc ids of size {}'.format(current_tmp_ids, sys.getsizeof(doc_ids)))
-    filer.ids_to_file(doc_ids, 'tmp_{}.ids'.format(current_tmp_ids))
+    print('>> Writing final.ids with doc ids of size {}'.format(sys.getsizeof(doc_ids)))
+    filer.ids_to_file(doc_ids, 'final.ids')
 
-    print('{:.2f} Processed up to doc_id: {}\nName: {}\nIndex Size: {}\nUnique Tokens: {}\n'.format(time.time() - start, current_doc_id, filename, sys.getsizeof(index), total_unique_tokens))
+    print('{:.2f} Processed up to doc_id: {}\nName: {}\nIndex Size: {}\nUnique Tokens: {}\n'.format(time.time() - start, current_doc_id, url, sys.getsizeof(index), total_unique_tokens))
 
     # Merging of indexes
     print('Merging indexes...\n')
---
+++ similar.py
@@ -0,0 +1,65 @@
+import hashlib
+
+def Similarity(dict1, dict2, threshold):
+    # print(dict1, dict2)
+    if _compute_similarity(create_simhash(dict1), create_simhash(dict2)) >= threshold:
+        return True
+    return False
+
+    
+def _compute_similarity(hash1, hash2):
+    # Retuns the % of similarity between two hashes
+    total_size = len(hash1)
+    similar = 0
+    for i in range(total_size):
+        if hash1[i] == hash2[i]:
+            similar += 1
+    
+    return similar/total_size
+
+def create_simhash(words_dict):
+    hash_digest_size = 64
+    
+    weights = []
+    for i in range(hash_digest_size * 8):
+        weights.append(int())
+
+    for word in words_dict:
+        hash = hashlib.blake2b(digest_size=hash_digest_size)
+        # print('Working with: {}'.format(word))
+        hash.update(word.encode('utf-8'))
+        hashed = hash.digest()
+        hashed_bytes = ensure_padding(''.join(format(int(b), 'b') for b in hashed), hash.digest_size * 8)
+        # print(hashed_bytes)
+        # print('>> Bytes: {}'.format(hashed_bytes))
+
+        for i in range(hash.digest_size * 8):
+            if bool(int(hashed_bytes[i])):
+                weights[i] += words_dict[word]
+            else:
+                weights[i] -= words_dict[word]
+        
+        # print('>> Weightings: {}'.format(weights))
+
+    # print('>> Weightings: {}'.format(weights))
+    # print('>> Normalized Weightings: {}'.format(self.normalize_weights(weights)))
+    # print()
+    return normalize_weights(weights)
+
+
+def ensure_padding(hashed_bytes, total_size):
+    if len(hashed_bytes) >= total_size:
+        return hashed_bytes
+    
+    extra_padding = total_size - len(hashed_bytes)    
+    return '0' * extra_padding + hashed_bytes
+
+def normalize_weights(weights):
+    normalized = weights
+    for i in range(len(normalized)):
+        if normalized[i] > 0:
+            normalized[i] = 1
+        else:
+            normalized[i] = 0
+    
+    return normalized
